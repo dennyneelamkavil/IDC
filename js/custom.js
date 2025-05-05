9571e5faf7ea8586377fe488705878df43afa1d9
Denny <dennynj4748@gmail.com>
2025-05-05 12:17:20 +0530

    updations to increase performance

--- js/custom.js
+++ js/custom.js
@@ -8,39 +8,39 @@ function getYear() {
 getYear();
 
 // nice select
-$(document).ready(function () {
-    $('select').niceSelect();
-});
+// $(document).ready(function () {
+//     $('select').niceSelect();
+// });
 
 // date picker
-$(function () {
-    $("#inputDate").datepicker({
-        autoclose: true,
-        todayHighlight: true
-    }).datepicker('update', new Date());
-});
+// $(function () {
+//     $("#inputDate").datepicker({
+//         autoclose: true,
+//         todayHighlight: true
+//     }).datepicker('update', new Date());
+// });
 
 // owl carousel slider js
-$('.team_carousel').owlCarousel({
-    loop: true,
-    margin: 15,
-    dots: true,
-    autoplay: true,
-    navText: [
-        '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-        '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-    ],
-    autoplayHoverPause: true,
-    responsive: {
-        0: {
-            items: 1,
-            margin: 0
-        },
-        576: {
-            items: 2,
-        },
-        992: {
-            items: 3
-        }
-    }
-})
+// $('.team_carousel').owlCarousel({
+//     loop: true,
+//     margin: 15,
+//     dots: true,
+//     autoplay: true,
+//     navText: [
+//         '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+//         '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+//     ],
+//     autoplayHoverPause: true,
+//     responsive: {
+//         0: {
+//             items: 1,
+//             margin: 0
+//         },
+//         576: {
+//             items: 2,
+//         },
+//         992: {
+//             items: 3
+//         }
+//     }
+// })
